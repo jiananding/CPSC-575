b540cac0b26d2eca4d1b987fb6c65eef2dcc90bf
Jianan Ding <jianan.ding1@ucalgary.ca>
2019-11-10 15:52:11 -0700

    update Camcat
    Update Camcat to midterm version

--- camcat/camcat/MainViewController.swift
+++ camcat/camcat/MainViewController.swift
@@ -15,8 +15,16 @@ class MainViewController: UIViewController {
     
     var expressionBar:UITextField!
     var operatorBarItem:[UIButton]! //Order: 0.Plus, 1.Minus, 2.Multiply, 3.Divide, 4.Undo
-    @IBOutlet weak var num_stack: UIScrollView!
-    
+    var addButton:UIButton!
+    var subButton:UIButton!
+    var multiButton:UIButton!
+    var divButton:UIButton!
+    var undoButton:UIButton!
+    var equalSign:UILabel!
+    var resultLabel:UILabel!
+    var num_stack: UIScrollView!
+    var backGround:UILabel!
+        
     var backend = BackEnd()
     
     override func viewDidLoad() {
@@ -38,6 +46,8 @@ class MainViewController: UIViewController {
         drawExpressionBar()
     }
     
+
+    
     func drawImageView(){
         imgView = UIImageView(image: imgData)
         imgView.contentMode = UIView.ContentMode.scaleAspectFit
@@ -47,37 +57,91 @@ class MainViewController: UIViewController {
     }
     
     func drawOperatorBar(){
+        addButton = getOperator(title: "+", x: 10, y: UIScreen.main.bounds.size.height - 130, width: UIScreen.main.bounds.size.width/5 - 20, height: 50)
+        addButton.addTarget(self, action: #selector(signButtonAction), for: .touchDown)
+        view.addSubview(addButton)
         
+        subButton = getOperator(title: "-", x: UIScreen.main.bounds.size.width/5 + 10, y: UIScreen.main.bounds.size.height - 130, width: UIScreen.main.bounds.size.width/5 - 20, height: 50)
+        subButton.addTarget(self, action: #selector(signButtonAction), for: .touchDown)
+        view.addSubview(subButton)
+        
+        multiButton = getOperator(title: "*", x: UIScreen.main.bounds.size.width/5*2 + 10, y: UIScreen.main.bounds.size.height - 130, width: UIScreen.main.bounds.size.width/5 - 20, height: 50)
+        multiButton.addTarget(self, action: #selector(signButtonAction), for: .touchDown)
+        view.addSubview(multiButton)
+        
+        divButton = getOperator(title: "/", x: UIScreen.main.bounds.size.width/5*3 + 10,
+                                y: UIScreen.main.bounds.size.height - 130, width: UIScreen.main.bounds.size.width/5 - 20, height: 50)
+        divButton.addTarget(self, action: #selector(signButtonAction), for: .touchDown)
+        view.addSubview(divButton)
+        
+        undoButton = getOperator(title: "←", x: UIScreen.main.bounds.size.width/5*4 + 10, y: UIScreen.main.bounds.size.height - 130, width: UIScreen.main.bounds.size.width/5 - 20, height: 50)
+        undoButton.addTarget(self, action: #selector(undoButtonAction(sender:)), for: .touchDown)
+        view.addSubview(undoButton)
+    }
+    
+    func getOperator(title:String, x:CGFloat, y:CGFloat, width:CGFloat, height:CGFloat) -> UIButton{
+        let btn:UIButton = UIButton(frame: CGRect(x: x, y: y, width: width, height: height))
+        btn.setTitle(title, for: UIControl.State.normal)
+        btn.setTitleColor(UIColor.white, for: UIControl.State.normal)
+        btn.titleLabel!.font = UIFont(name: "System", size: 500)
+        btn.layer.borderWidth = 1
+        btn.layer.cornerRadius = 10
+        btn.layer.borderColor = UIColor.gray.cgColor
+        btn.backgroundColor = UIColor.lightGray
+        return btn
     }
     
     func drawExpressionBar(){
-        expressionBar = UITextField(frame: CGRect(x: 0, y: 0, width: view.frame.size.width-CGFloat(50), height: 20))
+//        backGround = UILabel(frame: CGRect(x: 0, y: 0, width: view.frame.size.width-CGFloat(50), height: 30))
+//        backGround.backgroundColor = .systemBackground
+//        backGround.text = "abc"
+//        self.view.addSubview(backGround)
+        equalSign = UILabel(frame: CGRect(x: (view.frame.size.width-CGFloat(50)-100), y: 35, width: 40, height: 30))
+        equalSign.text = "     ="
+        equalSign.backgroundColor = .systemBackground
+        self.view.addSubview(equalSign)
+
+        expressionBar = UITextField(frame: CGRect(x: 0, y: 0, width: view.frame.size.width-CGFloat(50) - 100, height: 30))
         expressionBar.keyboardType = UIKeyboardType.decimalPad
-        expressionBar.center = CGPoint(x: view.frame.size.width/2, y: 50)
-        expressionBar.layer.cornerRadius=10;
-        expressionBar.layer.borderWidth=1;
-        expressionBar.layer.borderColor=UIColor.black.cgColor
+        expressionBar.center = CGPoint(x: (view.frame.size.width-CGFloat(50)-100)/2 + 20, y: 50)
+        expressionBar.layer.cornerRadius=10
+        expressionBar.layer.borderWidth=1
+        expressionBar.layer.borderColor=UIColor.darkGray.cgColor
+        expressionBar.backgroundColor = .systemBackground
         expressionBar.textAlignment = .center
-        expressionBar.text = "Equal: 0"
+        expressionBar.text = ""
         self.view.addSubview(expressionBar)
         let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 30))
         doneToolbar.barStyle = .default
 
-       let emptySpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
-       let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.doneButtonAction))
+        let emptySpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.doneButtonAction))
 
-       let items = [emptySpace, done]
-       doneToolbar.items = items
-       doneToolbar.sizeToFit()
+        let items = [emptySpace, done]
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
 
-       expressionBar.inputAccessoryView = doneToolbar
+        expressionBar.inputAccessoryView = doneToolbar
+        
+        
+        resultLabel = UILabel(frame: CGRect(x: (view.frame.size.width-CGFloat(50)-60), y: 35, width: (view.frame.size.width - (view.frame.size.width-CGFloat(50)-30)), height: 30))
+        resultLabel.text = "0"
+        resultLabel.backgroundColor = .systemBackground
+        self.view.addSubview(resultLabel)
     }
     
-    @objc func doneButtonAction(){
+    func updateResult() {
+        resultLabel.text = backend.result
+    }
+    
+    @objc func doneButtonAction() {
         expressionBar.resignFirstResponder()
+        expressionBar.text!.append(" ")
+        backend.calculation(expressionBar.text!)
+        updateResult()
     }
     
-    func prepareGestureRecog(){
+    func prepareGestureRecog() {
         print("prepareGestureRecog Called")
         imgView.isUserInteractionEnabled = true
         let pinchMethod = UIPinchGestureRecognizer(target: self, action: #selector(pinchImage(sender:)))    //Zoom in/out
@@ -170,9 +234,10 @@ class MainViewController: UIViewController {
 
     //---Add Num Button Action---
     func create_num_stack() {
+        num_stack = UIScrollView(frame: CGRect(x: 0, y: view.frame.height * 0.75, width: UIScreen.main.bounds.size.width, height: 50))
         num_stack.sizeToFit()
         num_stack.layoutIfNeeded()
-        num_stack.backgroundColor = .darkGray
+        num_stack.backgroundColor = .systemBackground
         var contentWidth: CGFloat = 0.0
         
         let length = backend.value.count
@@ -184,16 +249,35 @@ class MainViewController: UIViewController {
             num_button.frame = CGRect(x: width * CGFloat(i) , y: 0, width: width, height: 50)
             num_button.setTitle(String(backend.value[i]), for: .normal)
             num_button.addTarget(self, action: #selector(buttonAction), for: .touchDown)
-            num_button.backgroundColor = UIColor.darkGray
+//            num_button.backgroundColor = UIColor.darkGray
             num_stack.addSubview(num_button)
         }
         num_stack.contentSize = CGSize(width: contentWidth, height: 50)
+        self.view.addSubview(num_stack)
     }
     
     @objc func buttonAction(sender: UIButton!) {
-        print("Button tapped")
-        //backend.equation.append(sender.titleLabel!.text! + " ")
-        //backend.calculation()
+        expressionBar.text?.append(sender.titleLabel!.text! + " ")
+        backend.calculation(expressionBar.text!)
+        updateResult()
+    }
+    
+    @objc func signButtonAction(sender: UIButton!) {
+        expressionBar.text?.append(sender.titleLabel!.text! + " ")
+        backend.calculation(expressionBar.text!)
+        updateResult()
+    }
+    
+    @objc func undoButtonAction(sender: UIButton!) {
+        var text = expressionBar.text?.split(separator: " ")
+        if text!.count >= 1 {
+            text?.removeLast()
+            var full_text = text?.joined(separator: " ")
+            full_text?.append(" ")
+            expressionBar.text = full_text
+            backend.calculation(expressionBar.text!)
+            updateResult()
+        }
     }
     //---End Num Button Action---
     
