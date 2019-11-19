d608bb5fe9decee7b3e13b79c581e8862ff764e9
Jianan Ding <jianan.ding1@ucalgary.ca>
2019-11-18 23:52:41 -0700

    Clean up files

--- Currency_exchange/Currency_exchange/ViewController.swift
+++ Currency_exchange/Currency_exchange/ViewController.swift
@@ -9,51 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // restrict textfile to input number or check textfile.text
+    // currency selection maybe can use picker view to do selection
+    
     @IBOutlet weak var init_value: UITextField!
     @IBOutlet weak var converted_value: UILabel!
+    
     
     var base: String = "CAD"
     var target: String = "USD"
     var date: String = ""
     var exchangeRates: [String: Double] =
-        ["AUD": 0.0,
-         "BGN": 0.0,
-         "BRL": 0.0,
-         "CAD": 0.0,
-         "CHF": 0.0,
-         "CNY": 0.0,
-         "CZK": 0.0,
-         "DKK": 0.0,
-         "GBP": 0.0,
-         "HKD": 0.0,
-         "HRK": 0.0,
-         "HUF": 0.0,
-         "IDR": 0.0,
-         "ILS": 0.0,
-         "INR": 0.0,
-         "ISK": 0.0,
-         "JPY": 0.0,
-         "KRW": 0.0,
-         "MXN": 0.0,
-         "MYR": 0.0,
-         "NOK": 0.0,
-         "NZD": 0.0,
-         "PHP": 0.0,
-         "PLN": 0.0,
-         "RON": 0.0,
-         "RUB": 0.0,
-         "SEK": 0.0,
-         "SGD": 0.0,
-         "THB": 0.0,
-         "TRY": 0.0,
-         "USD": 0.0,
-         "ZAR": 0.0
-        ]
+        ["AUD": 0.0, "BGN": 0.0, "BRL": 0.0, "CAD": 0.0, "CHF": 0.0, "CNY": 0.0, "CZK": 0.0,
+         "DKK": 0.0, "GBP": 0.0, "HKD": 0.0, "HRK": 0.0, "HUF": 0.0, "IDR": 0.0, "ILS": 0.0,
+         "INR": 0.0, "ISK": 0.0, "JPY": 0.0, "KRW": 0.0, "MXN": 0.0, "MYR": 0.0, "NOK": 0.0,
+         "NZD": 0.0, "PHP": 0.0, "PLN": 0.0, "RON": 0.0, "RUB": 0.0, "SEK": 0.0, "SGD": 0.0,
+         "THB": 0.0, "TRY": 0.0, "USD": 0.0, "ZAR": 0.0]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // converted value change while textfield text editing changed
         init_value.addTarget(self, action: #selector(textFieldDidChange(_:)), for: UIControl.Event.editingChanged)
         getLatest()
     }
@@ -74,7 +49,7 @@ class ViewController: UIViewController {
                 
             do {
                 guard let dict = try JSONSerialization.jsonObject(with: data, options: []) as? [String: AnyObject] else {return}
-                print(dict.description)
+                // print(dict.description)
                 guard let rates = dict["rates"] as? [String: Double], let base = dict["base"] as? String, let date = dict["date"] as? String else {return}
                 let currencies = rates.keys.sorted()
                 for currency in currencies {
