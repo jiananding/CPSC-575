156e18b27d3a371bf272bbcd4c87145c5115958a
Jianan Ding <jianan.ding1@ucalgary.ca>
2019-11-26 21:39:00 -0700

    update Currency exchange

--- Currency_exchange/Currency_exchange/PickerLauncher.swift
+++ Currency_exchange/Currency_exchange/PickerLauncher.swift
@@ -1,78 +1,75 @@
+////
+////  PickerLauncher.swift
+////  Currency_exchange
+////
+////  Created by stephen on 2019-11-23.
+////  Copyright © 2019 Stephen. All rights reserved.
+////
 //
-//  PickerLauncher.swift
-//  Currency_exchange
+//// Reference: youtube.com/watch?v=2kwCfFG5fDA
 //
-//  Created by stephen on 2019-11-23.
-//  Copyright © 2019 Stephen. All rights reserved.
+//import Foundation
+//import UIKit
 //
-
-// Reference: youtube.com/watch?v=2kwCfFG5fDA
-
-import Foundation
-import UIKit
-
-class PickerLauncher: NSObject, UITableViewDataSource, UITableViewDelegate {
-    
-    var main_view = UIView()
-    let blackView = UIView()
-
-    let menu = UITableView()
-    
-    var selection: [String] =
-        ["AUD", "BGN", "BRL", "CAD", "CHF", "CNY", "CZK", "DKK", "GBP", "HKD", "HRK", "HUF", "IDR", "ILS",
-         "INR", "ISK", "JPY", "KRW", "MXN", "MYR", "NOK", "NZD", "PHP", "PLN", "RON", "RUB", "SEK", "SGD",
-         "THB", "TRY", "USD", "ZAR"]
-    
-    func showPicker(own_view: UIView) {
-        self.main_view = own_view
-        
-        blackView.backgroundColor = UIColor(white: 0, alpha: 0.5)
-        blackView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleDismiss)))
-        
-        main_view.addSubview(blackView)
-        main_view.addSubview(menu)
-        
-        let height: CGFloat = 500
-        let y: CGFloat = main_view.frame.height - height
-        menu.frame = CGRect(x: 0, y: main_view.frame.height, width: main_view.frame.width, height: height)
-        menu.dataSource = self
-        menu.delegate = self
-        menu.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        
-        blackView.frame = main_view.frame
-        blackView.alpha = 0
-        
-        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
-            self.blackView.alpha = 1
-            self.menu.frame = CGRect(x: 0, y: y, width: self.menu.frame.width, height: self.menu.frame.height)
-        }, completion: nil)
-    }
-    
-    // react to the gesture for black view to dismiss the black view
-    @objc func handleDismiss() {
-        UIView.animate(withDuration: 0.5) {
-            self.blackView.alpha = 0
-            
-            self.menu.frame = CGRect(x: 0.0, y: self.main_view.frame.height, width: self.menu.frame.width, height: self.menu.frame.height)
-        }
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return selection.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = selection[indexPath.row]
-        cell.textLabel?.numberOfLines = 0
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 50.0
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-    }
-}
+//class PickerLauncher: NSObject, UITableViewDataSource, UITableViewDelegate {
+//    
+//    var main_view = UIView()
+//    var blackView = UIView()
+//
+//    let menu = UITableView()
+//    
+//
+//    
+//    func showPicker(own_view: UIView) {
+//        self.main_view = own_view
+//        
+//        blackView.backgroundColor = UIColor(white: 0, alpha: 0.5)
+//        blackView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleDismiss)))
+//        
+//        main_view.addSubview(blackView)
+//        main_view.addSubview(menu)
+//        
+//        let height: CGFloat = 500
+//        let y: CGFloat = main_view.frame.height - height
+//        menu.frame = CGRect(x: 0, y: main_view.frame.height, width: main_view.frame.width, height: height)
+//        menu.dataSource = self
+//        menu.delegate = self
+//        menu.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+//        
+//        blackView.frame = main_view.frame
+//        blackView.alpha = 0
+//        
+//        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+//            self.blackView.alpha = 1
+//            self.menu.frame = CGRect(x: 0, y: y, width: self.menu.frame.width, height: self.menu.frame.height)
+//        }, completion: nil)
+//    }
+//    
+//    // react to the gesture for black view to dismiss the black view
+//    @objc func handleDismiss() {
+//        UIView.animate(withDuration: 0.5) {
+//            self.blackView.alpha = 0
+//            
+//            self.menu.frame = CGRect(x: 0.0, y: self.main_view.frame.height, width: self.menu.frame.width, height: self.menu.frame.height)
+//        }
+//    }
+//    
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return selection.count
+//    }
+//    
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+//        cell.textLabel?.text = selection[indexPath.row]
+//        cell.textLabel?.numberOfLines = 0
+//        return cell
+//    }
+//    
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 50.0
+//    }
+//    
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        
+//    }
+//}
--- Currency_exchange/Currency_exchange/ViewController.swift
+++ Currency_exchange/Currency_exchange/ViewController.swift
@@ -8,15 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
     @IBOutlet weak var base_button: UIButton!
     @IBOutlet weak var target_button: UIButton!
     
     @IBOutlet weak var init_value: UITextField!
     @IBOutlet weak var converted_value: UILabel!
-    
-    @IBOutlet weak var base_picker: UIScrollView!
-    @IBOutlet weak var target_picker: UIScrollView!
         
     var date: String = ""
 
@@ -26,6 +23,11 @@ class ViewController: UIViewController {
          "INR": 0.0, "ISK": 0.0, "JPY": 0.0, "KRW": 0.0, "MXN": 0.0, "MYR": 0.0, "NOK": 0.0,
          "NZD": 0.0, "PHP": 0.0, "PLN": 0.0, "RON": 0.0, "RUB": 0.0, "SEK": 0.0, "SGD": 0.0,
          "THB": 0.0, "TRY": 0.0, "USD": 0.0, "ZAR": 0.0]
+    
+    var selection: [String] =
+        ["AUD", "BGN", "BRL", "CAD", "CHF", "CNY", "CZK", "DKK", "GBP", "HKD", "HRK", "HUF", "IDR", "ILS",
+         "INR", "ISK", "JPY", "KRW", "MXN", "MYR", "NOK", "NZD", "PHP", "PLN", "RON", "RUB", "SEK", "SGD",
+         "THB", "TRY", "USD", "ZAR"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -57,8 +59,10 @@ class ViewController: UIViewController {
                     self.exchangeRates.updateValue(rate!, forKey: currency)
                 }
                 OperationQueue.main.addOperation({
-                    self.calculate(init_val: 1.0)
-                    self.date = date
+                    //self.calculate(init_val: 1.0)
+                    let val = Double(self.init_value.text!)
+                    self.calculate(init_val: val!)
+                    //self.date = date
                 })
             }
             catch {
@@ -82,14 +86,80 @@ class ViewController: UIViewController {
         }
     }
     
-    let pickerLauncher = PickerLauncher()
-    
-    @IBAction func base_button_label(_ sender: Any) {
-        pickerLauncher.showPicker(own_view: view)
+    let menu = UITableView()
+    var blackView = UIView()
+
+    @IBAction func base_button_label(_ sender: UIButton) {
+        showPicker(own_view: view, sender: sender)
     }
     
-    @IBAction func target_button_label(_ sender: Any) {
-        pickerLauncher.showPicker(own_view: view)
+    @IBAction func target_button_label(_ sender: UIButton) {
+        showPicker(own_view: view, sender: sender)
+    }
+    
+    func showPicker(own_view: UIView, sender: UIButton) {
+        blackView.backgroundColor = UIColor(white: 0, alpha: 0.5)
+        blackView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleDismiss)))
+        
+        own_view.addSubview(blackView)
+        own_view.addSubview(menu)
+        
+        let height: CGFloat = 500
+        let y: CGFloat = own_view.frame.height - height
+        menu.frame = CGRect(x: 0, y: own_view.frame.height, width: own_view.frame.width, height: height)
+        menu.dataSource = self
+        menu.delegate = self
+        menu.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        menu.tag = sender == base_button ? 1 : 2    // 1: base_button, 2: target_button
+        
+        blackView.frame = own_view.frame
+        blackView.alpha = 0
+        
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            self.blackView.alpha = 1
+            self.menu.frame = CGRect(x: 0, y: y, width: self.menu.frame.width, height: self.menu.frame.height)
+        }, completion: nil)
+    }
+    
+    // react to the gesture for black view to dismiss the black view
+    @objc func handleDismiss() {
+        UIView.animate(withDuration: 0.5) {
+            self.blackView.alpha = 0
+            
+            self.menu.frame = CGRect(x: 0.0, y: self.view.frame.height, width: self.menu.frame.width, height: self.menu.frame.height)
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return selection.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = selection[indexPath.row]
+        cell.textLabel?.numberOfLines = 0
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50.0
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if (tableView.tag == 1) {
+            // base_button
+            base_button.titleLabel?.text = selection[indexPath.row]
+//            let val = Double(init_value.text!)
+//            calculate(init_val: val!)
+        }
+        else {
+            // target_button
+            target_button.titleLabel?.text = selection[indexPath.row]
+//
+        }
+        getLatest()
+        //handleDismiss()
+
     }
 }
 
